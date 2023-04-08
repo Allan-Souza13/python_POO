dd8b01d4646e44dca0deae1c1df132b322d97898
Allan Souza <vejeta300@hotmail.com>
2023-04-08 13:29:35 -0300

    Ler entrada e saida

--- conta.py
+++ conta.py
@@ -3,11 +3,20 @@
 #receita de bolo
 class Conta:
 
-    def __init__(self,numero,titular,saldo,limite):#função construtora, que constroe o objeto
+    def __init__(self,numero,titular,saldo,limite):#função construtora, que constroe o objeto/inicialização
         print("Construindo objeto...{}".format(self))#self é a referença desse objeto que guardou o endereço
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("saldo de {} do titular {} ".format(self.saldo,self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
 
 
